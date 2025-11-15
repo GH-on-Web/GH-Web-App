06a217123507bb175245f121c540a1974d624e8b
[Nico Azel] <nazel@thorntontomasetti.com>
2025-11-15 12:04:35 -0500

    Create Front End

---
+++ frontend/src/store/workspaceStore.js
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import { nanoid } from 'nanoid';
+import { addEdge, applyNodeChanges, applyEdgeChanges } from 'reactflow';
+
+const initialNodes = [
+  {
+    id: nanoid(),
+    position: { x: 0, y: 0 },
+    data: { label: 'Node A' },
+  },
+  {
+    id: nanoid(),
+    position: { x: 200, y: 100 },
+    data: { label: 'Node B' },
+  },
+];
+
+const initialEdges = [];
+
+const useWorkspaceStore = create((set, get) => ({
+  nodes: initialNodes,
+  edges: initialEdges,
+  onNodesChange: (changes) =>
+    set({ nodes: applyNodeChanges(changes, get().nodes) }),
+  onEdgesChange: (changes) =>
+    set({ edges: applyEdgeChanges(changes, get().edges) }),
+  onConnect: (connection) =>
+    set({ edges: addEdge(connection, get().edges) }),
+}));
+
+export default useWorkspaceStore;
